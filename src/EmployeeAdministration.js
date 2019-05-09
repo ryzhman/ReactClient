f5f4ff92ff21559836073be837832c8d90b32d53
Oleksandr Ryzhkov <oryzhkov@paymentus.com>
2019-05-09 00:26:21 -0400

    Start React client

---
+++ src/EmployeeAdministration.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class EmployeeAdministration extends Component {
+    render() {
+        return (
+            <h1>Employee Administration Application</h1>
+        )
+    }
+}
+
+export default EmployeeAdministration
